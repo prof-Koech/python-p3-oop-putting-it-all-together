04fb4d1c66d1b2d75b4bf851a2ee74ce844f4d82
Kennedy <kiplangatkennedy019@gmail.com>
2023-05-22 14:46:13 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = 'Insert title', page_count = 0):
+        self._title = title
+        self._page_count = page_count
     
+    def get_title(self):
+        return self._title
+    def set_title(self, title):
+        self._title = title
+        print('Title has been changed')
+
+    def get_page_count(self):
+        return self._page_count
+    def set_page_count(self, page_count):
+        if type(page_count) != int :
+            print('page_count must be an integer')
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "Insert brand", size = 0):
+        self.brand = brand
+        self._size = size
+
+    def set_size(self, size):
+        if type(size) != int: 
+            print('size must be an integer')
+        else:
+            self._size = size
+    def get_size(self):
+        return self._size
+    size = property(get_size, set_size)
+    
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        
+    condition = 'New'
